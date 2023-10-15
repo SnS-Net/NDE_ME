039a26f4654ad5e4364b56ec511023af62ec8299
SnS-Net <143277412+SnS-Net@users.noreply.github.com>
2023-10-15 16:51:18 -0400

    Update create_NDE_ME.sh

--- create_NDE_ME.sh
+++ create_NDE_ME.sh
@@ -1,51 +1,50 @@
 #!/bin/bash
-done="------\nDONE!\n------"
 
-echo 'Creating the main directory'
+# Colors for echo statements
+GREEN='\e[32m'
+YELLOW='\e[33m'
+NC='\e[0m'  # No color
+
+# Create the NDE_ME directory
+echo -e "${GREEN}Creating NDE_ME directory...${NC}"
 mkdir -p NDE_ME
 
-echo 'Enter the main directory'
-cd NDE_ME
-echo "$done"
+# Create Python directory structure
+echo -e "${YELLOW}Creating Python directory structure...${NC}"
+mkdir -p NDE_ME/python/runbooks/linux_config
+mkdir -p NDE_ME/python/runbooks/cisco_config
+mkdir -p NDE_ME/python/templates/linux
+mkdir -p NDE_ME/python/templates/cisco
+mkdir -p NDE_ME/python/inventory
+touch NDE_ME/python/inventory/inventory.ini
+touch NDE_ME/python/README.md
 
-echo 'Creating subdirectories'
-mkdir -p ansible/{roles/linux_config,roles/cisco_config} python/{runbooks/linux_config,runbooks/cisco_config,templates/linux,templates/cisco} bash/{automation_scripts,script_libraries,sample_configurations/linux_config,sample_configurations/cisco_config} docs
-sleep 2
-echo "$done"
+# Create Bash directory structure
+echo -e "${YELLOW}Creating Bash directory structure...${NC}"
+mkdir -p NDE_ME/bash/automation_scripts
+touch NDE_ME/bash/automation_scripts/monitoring.sh
+touch NDE_ME/bash/automation_scripts/backup.sh
+touch NDE_ME/bash/automation_scripts/log_analysis.sh
+touch NDE_ME/bash/automation_scripts/config_management.sh
+mkdir -p NDE_ME/bash/script_libraries
+touch NDE_ME/bash/script_libraries/utils.sh
+mkdir -p NDE_ME/bash/sample_configurations/linux_config
+mkdir -p NDE_ME/bash/sample_configurations/cisco_config
+touch NDE_ME/bash/README.md
+touch NDE_ME/bash/best_practices.md
+touch NDE_ME/bash/integration_guide.md
 
-echo 'Creating 5 empty README files'
-touch README.md && echo '1'
-touch ansible/README.md && echo '2'
-touch python/README.md && echo '3'
-touch bash/README.md && echo '4'
-touch docs/README.md && echo '5'
-echo "$done"
+# Create Docs directory
+echo -e "${GREEN}Creating Docs directory...${NC}"
+mkdir -p NDE_ME/docs
+touch NDE_ME/docs/user_guide.md
+touch NDE_ME/docs/developer_guide.md
 
-Create empty configuration files
-touch ansible/ansible.cfg
-touch ansible/inventory/inventory.ini
-touch bash/best_practices.md
-touch bash/integration_guide.md
-touch docs/user_guide.md
-touch docs/developer_guide.md
-echo "$done"
+# Create main README and other files
+echo -e "${GREEN}Creating main README and other files...${NC}"
+touch NDE_ME/README.md
+touch NDE_ME/LICENSE
+touch NDE_ME/.gitignore
+touch NDE_ME/.gitattributes
 
-# Sample code placeholders (you can replace these with actual code)
-echo "# Ansible Linux Configuration" > ansible/roles/linux_config/tasks/main.yml
-echo "# Ansible Cisco Configuration" > ansible/roles/cisco_config/tasks/main.yml
-echo "# Python Runbook for Linux Configuration" > python/runbooks/linux_config/main.py
-echo "# Python Runbook for Cisco Configuration" > python/runbooks/cisco_config/main.py
-echo "# Bash Automation Script for Monitoring" > bash/automation_scripts/monitoring.sh
-echo "# Bash Automation Script for Backup" > bash/automation_scripts/backup.sh
-echo "# Bash Automation Script for Log Analysis" > bash/automation_scripts/log_analysis.sh
-echo "# Bash Automation Script for Configuration Management" > bash/automation_scripts/config_management.sh
-echo "$done"
-
-echo "# Bash Script Library" > bash/script_libraries/utils.sh
-echo "# Sample Linux Configuration" > bash/sample_configurations/linux_config/sample.conf
-echo "# Sample Cisco Configuration" > bash/sample_configurations/cisco_config/sample.conf
-echo "# User Guide" > docs/user_guide.md
-echo "# Developer Guide" > docs/developer_guide.md
-echo "$done"
-
-echo "NDE_ME directory structure created with sample code placeholders."
+echo -e "${GREEN}Directory structure created successfully!${NC}"
